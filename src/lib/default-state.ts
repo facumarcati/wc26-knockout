493f5841f3936f7228fb4547f6696854cccd5f3f
facumarcati <facumarcati14@gmail.com>
2026-06-30 16:05:42 -0300

    Agrego resultado CIV-NOR

--- src/lib/default-state.ts
+++ src/lib/default-state.ts
@@ -1,11 +1,14 @@
 import type { DrawState } from "./drawState";
 
 export const DEFAULT_DRAW_STATE: DrawState = {
-  "v": 1,
-  "winners": {
+  v: 1,
+  winners: {
     "0-pair-0": "BRA",
+    "0-pair-1": "NOR",
+    // "0-pair-2": "MEX" "ECU",
     "0-pair-12": "MAR",
     "0-pair-13": "CAN",
-    "0-pair-15": "PRY"
-  }
-}
+    // "0-pair-14": "FRA" "SWE",
+    "0-pair-15": "PRY",
+  },
+};

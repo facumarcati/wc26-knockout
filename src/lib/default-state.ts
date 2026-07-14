8d7f363eb199df8589ef907e68e66b0cf767501b
facumarcati <facumarcati14@gmail.com>
2026-07-14 18:30:32 -0300

    Actualizo resultados

--- src/lib/default-state.ts
+++ src/lib/default-state.ts
@@ -33,5 +33,8 @@ export const DEFAULT_DRAW_STATE: DrawState = {
     "3-pair-1": "ARG",
     "3-pair-2": "ESP",
     "3-pair-3": "FRA",
+
+    // "4-pair-0": "ARG" "ENG",
+    "4-pair-1": "ESP",
   },
 };
